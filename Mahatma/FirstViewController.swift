04c35fe9121dd36a45f91a135840f477bddd7b35
Johan Cornelissen <johan1252@gmail.com>
2018-02-02 22:40:21 -0500

    Initial Commit

---
+++ Mahatma/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Mahatma
+//
+//  Created by Johan Cornelissen on 2018-02-02.
+//  Copyright © 2018 Johan Cornelissen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Mahatma/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Mahatma
+//
+//  Created by Johan Cornelissen on 2018-02-02.
+//  Copyright © 2018 Johan Cornelissen. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
